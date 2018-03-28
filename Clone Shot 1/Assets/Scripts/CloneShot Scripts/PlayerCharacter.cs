e39e2075f5fe99b6bf7843eb8cd03a25b2124efb
Nick Richardson <nickrnky@gmail.com>
2018-03-28 15:23:44 -0400

    Added network components
    Added the basic required components for networking

---
+++ Clone Shot 1/Assets/Scripts/CloneShot Scripts/PlayerCharacter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter : MonoBehaviour 
+{
+	private int _health;
+    CharacterController cc;
+
+    void Start() 
+    {
+		_health = 5;
+        cc = (CharacterController)GetComponent<CharacterController>();
+        //if (cc.isTrigger)
+        //    Debug.Log("Palyer's Character controller is a trigger");
+        //else
+         //   Debug.Log("Palyer's Character controller is not a trigger");
+
+    }
+
+	public void Hurt(int damage) 
+    {
+		_health -= damage;
+		Debug.Log("Health: " + _health);
+        //if (cc.isTrigger)
+        //    Debug.Log("Palyer's Character controller is a trigger");
+        //else
+        //    Debug.Log("Palyer's Character controller is not a trigger");
+
+    }
+}

ed405e5242117148c709b2cbf0b3f252c5d9e05f
plot720 <36005346+plot720@users.noreply.github.com>
2018-04-16 18:48:39 -0400

    Checking in just to pull
    Unity craziness

--- Clone Shot 1/Assets/Scripts/Shoot.cs
+++ Clone Shot 1/Assets/Scripts/Shoot.cs
@@ -28,36 +28,18 @@ namespace Assets.Scripts
                 return;
             }
             StartCoroutine(Life());
-            
+            //https://answers.unity.com/questions/690186/bullet-management-possible-without-rigidbody.html
 
             Vector3 vectorToTarget = PointToTravelTo - transform.position;
             float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
             Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
             transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
-            //Debug.Log(YRotation);
-
-            //bool neg = true;
-            //if (YRotation > 270)
-            //{
-            //    neg = false;
-            //    YRotation -= 270;
-            //    YRotation = 90 - YRotation;
-            //}
-
-            //YSpeed = 30 * YRotation / 90;
-
-            //if(neg)
-            //{
-            //    YSpeed *= -1;
-            //}
 
         }
 
         void Update()
         {
             transform.position += transform.forward * speed * Time.deltaTime;
-
-            //transform.Translate(0, YSpeed * Time.deltaTime, speed * Time.deltaTime);
         }
 
         IEnumerator Life()
